3dbc61b8cdbce1a0098f76c18352c6c446e0223d
LinnEriksson89 <123004529+LinnEriksson89@users.noreply.github.com>
2025-12-21 16:15:59 +0100

    router changes

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,28 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '@/views/HomeView.vue'
+import MoviesView from '@/views/MoviesView.vue'
+import AboutVueView from '@/views/AboutVueView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+        path: "/",
+        name: "start",
+        component: HomeView
+
+    },
+    {
+        path: "/movies",
+        name: "filmer",
+        component: MoviesView
+    },
+    {
+      path: "/aboutvue",
+      name: "om vue",
+      component: AboutVueView,
+    },
+  ],
 })
 
 export default router
