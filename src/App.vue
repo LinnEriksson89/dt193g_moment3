d5d62c555ba72ec374b1be61a747ee3f8fd11782
LinnEriksson89 <123004529+LinnEriksson89@users.noreply.github.com>
2025-12-21 16:21:54 +0100

    General styling

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,35 @@
-<script setup></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+    <div id="background">
+        <div id="wrapper">
+            <Header />
+            <main>
+                <router-view />
+            </main>
+            <Footer />
+        </div>
+    </div>
 </template>
 
-<style scoped></style>
+<script setup>
+import Header from "./components/Header.vue"
+import Footer from "./components/Footer.vue"
+import { RouterView } from 'vue-router'
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Flamenco:wght@300;400&family=Kalam:wght@300;400;700&display=swap');
+
+#wrapper {
+    max-width: 1000px;
+    background-color: #f1f1f1;
+    margin: auto;
+    padding: 2%;
+}
+
+#background {
+    background-color: #800;
+    margin: 0;
+    border: 1px solid #000;
+    padding: 0;
+}
+</style>
