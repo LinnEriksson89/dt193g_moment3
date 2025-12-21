1f2261fb726094ff642cca54efc3bb3c226a0f76
LinnEriksson89 <123004529+LinnEriksson89@users.noreply.github.com>
2025-12-21 23:05:45 +0100

    adding home view

---
+++ src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<template>
+    <h2>Välkommen!</h2>
+    <p>Det här är en sida som handlar om att titta på film. Här tänkte jag lista olika filmer jag har sett eller vill
+        se.</p>
+</template>
+
+<script setup></script>
+
+<style scoped>
+h2 {
+    font-size: 1.6em;
+    font-weight: bold;
+    font-family: Kalam, Arial, Helvetica, sans-serif;
+    margin: 2%;
+    border-bottom: #800 2px solid;
+}
+
+p {
+    font-family: Georgia, serif;
+    line-height: 1.6;
+    margin: 2%;
+    max-width: 600px;
+}
+</style>
