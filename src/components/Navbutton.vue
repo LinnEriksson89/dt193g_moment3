07fd2adb5b6d73edc1c70c89c815d975644a07de
LinnEriksson89 <123004529+LinnEriksson89@users.noreply.github.com>
2025-12-21 16:21:22 +0100

    Adding navigation

---
+++ src/components/Navbutton.vue
@@ -0,0 +1,35 @@
+<template>
+  <a v-bind:href="props.link" :class="{ active: isActive}">
+    <li>{{ props.title }}</li>
+  </a>
+</template>
+
+<script setup>
+const props = defineProps(["title", "link"]);
+</script>
+
+<style scoped>
+    a {
+        font-weight: bold;
+        text-decoration: none;
+        text-align: center;
+        color: #f1f1f1;
+        background-color: #800;
+        display: inline-block;
+        border: 2px solid #f1f1f1;
+        border-radius: 10px;
+        margin: 4px;
+        padding: 10px 45px;
+
+        &:visited {
+            color: #f1f1f1;
+        }
+
+        &:hover,
+        &:active {
+            background-color: #f1f1f1;
+            color: #800;
+            border: 2px solid #800;
+        }
+    }
+</style>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,19 @@
+<template>
+  <ul>
+    <Navbutton title="Start" link="/" />
+    <Navbutton title="Filmer" link="/movies" />
+    <Navbutton title="Om Vue" link="/aboutvue" />
+  </ul>
+</template>
+
+<script setup>
+import Navbutton from "./Navbutton.vue";
+</script>
+
+<style scoped>
+    ul {
+        padding: 0;
+        list-style: none;
+        text-align: center;
+    }
+</style>
