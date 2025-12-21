fa586db3a6beaf19c6c4a4d8d2bd792ab995763d
LinnEriksson89 <123004529+LinnEriksson89@users.noreply.github.com>
2025-12-21 16:21:08 +0100

    Adding header

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1>Linns Filmsida</h1>
+    <Navigation />
+</template>
+
+<script setup>
+import Navigation from "./Navigation.vue";
+</script>
+
+<style scoped>
+h1 {
+    font-size: 2.4em;
+    font-weight: bold;
+    font-family: Kalam, Arial, Helvetica, sans-serif;
+    text-align: center;
+    margin: 2%;
+    border-bottom: #800 2px solid;
+}
+</style>
