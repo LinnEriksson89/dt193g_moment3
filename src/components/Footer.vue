e24026eb4207b71b711d523f23151834f5e8bd32
LinnEriksson89 <123004529+LinnEriksson89@users.noreply.github.com>
2025-12-21 16:20:55 +0100

    Footer added

---
+++ src/components/Footer.vue
@@ -0,0 +1,28 @@
+<template>
+  <footer>
+    <p>
+      Webbplats skapad som en del av
+      <a
+        href="https://www.miun.se/utbildning/kursplaner-och-utbildningsplaner/DT193G/"
+        >Fullstack-utveckling med ramverk</a>
+      av <a href="mailto:lier2209@student.miun.se">Linn Eriksson</a>.
+    </p>
+  </footer>
+</template>
+
+<script setup></script>
+
+<style scoped>
+    footer {
+        border-top: 2px solid #800;
+        padding: 2%;
+    }
+
+    p {
+        text-align: center;
+    }
+
+    a {
+        color: #800;
+    }
+</style>
